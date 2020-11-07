7350bef52fcb03614b6aa1ea0504436ec50f750c
Melvin Aziz <58566746+MelvAziz@users.noreply.github.com>
2020-11-07 16:53:29 -0500

    commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,92 @@
-import { genres } from "./genre";
-import { DOMSelectors } from "./DOM";
+
+const DOMSelectors = {
+  grid: document.querySelector(".movie-grid"),
+  searchForm: document.getElementById("search-form"),
+  searchArea: document.getElementById("search-area"),
+};
+
 const key = `1fd276ec57b4baedacae00246e5cf4b7`;
 const query = `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=1&vote_count.gte=10000&vote_average.gte=8`;
+
+const genres = [
+  {
+    id: 28,
+    name: "Action",
+  },
+  {
+    id: 12,
+    name: "Adventure",
+  },
+  {
+    id: 16,
+    name: "Animation",
+  },
+  {
+    id: 35,
+    name: "Comedy",
+  },
+  {
+    id: 80,
+    name: "Crime",
+  },
+  {
+    id: 99,
+    name: "Documentary",
+  },
+  {
+    id: 18,
+    name: "Drama",
+  },
+  {
+    id: 10751,
+    name: "Family",
+  },
+  {
+    id: 14,
+    name: "Fantasy",
+  },
+  {
+    id: 36,
+    name: "History",
+  },
+  {
+    id: 27,
+    name: "Horror",
+  },
+  {
+    id: 10402,
+    name: "Music",
+  },
+  {
+    id: 9648,
+    name: "Mystery",
+  },
+  {
+    id: 10749,
+    name: "Romance",
+  },
+  {
+    id: 878,
+    name: "Science Fiction",
+  },
+  {
+    id: 10770,
+    name: "TV Movie",
+  },
+  {
+    id: 53,
+    name: "Thriller",
+  },
+  {
+    id: 10752,
+    name: "War",
+  },
+  {
+    id: 37,
+    name: "Western",
+  },
+];
+
 
 const init = async function () {
   try {
